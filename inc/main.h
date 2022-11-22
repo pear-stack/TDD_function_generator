459801466e3cdd377c7cf20a66ba503f441da653
p3ar-stack <grusza.adrian@gmail.com>
2022-11-22 15:53:58 +0100

    CMake + OpenOCD

---
+++ inc/main.h
@@ -0,0 +1,14 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f303xe.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+void delay_ms(uint64_t delay){
+	for(uint64_t i = 0; i < delay*169; i++){
+	}
+}
+
+int main(void)
+{
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+	GPIOA->MODER |= 1 << 10;
+	GPIOA->OSPEEDR |= 1 << 10;
+	while(1) 
+	{
+		delay_ms(1000);
+		GPIOA->ODR |= 1 << 5;
+		delay_ms(1000);
+		GPIOA->ODR &= ~(1 << 5);
+	}
+}
+
+
+
+
+
+
